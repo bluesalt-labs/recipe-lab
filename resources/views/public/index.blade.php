2560b792bc6dd329cf0d889444ddd8ad740f53f8
Unknown <luke@bluesaltlabs.com>
2019-03-30 14:25:07 -0700

    Fixes font sizing issues. Replaces default font family. Adds lorem ipsum to the home page to test fonts.

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -5,7 +5,30 @@
 
 @section('content')
     <div class="container mx-auto">
-        <h1>Welcome to Recipe Lab</h1>
-        <p class="mt-5">This is the home page. Things are getting set up so please check back soon!</p>
+        <h1 class="mt-5 mb-2">Welcome to Recipe Lab</h1>
+        <p class="mt-3">This is the home page. Things are getting set up so please check back soon!</p>
+
+        <p class="mt-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis dui, fermentum ac tortor eget,
+            accumsan vehicula ante. Sed ultricies enim vel sem feugiat rutrum. Duis lacinia turpis vel nulla interdum
+            euismod. Maecenas et eros a arcu venenatis vulputate nec vel nulla. In sit amet purus sed diam mollis
+            fringilla. Curabitur mollis tortor est, non auctor eros bibendum eu. Interdum et malesuada fames ac ante
+            ipsum primis in faucibus. Etiam interdum metus eget ligula lacinia mattis. Nunc semper vestibulum orci,
+            pellentesque finibus est consectetur eu. Etiam id tempor diam, at dignissim sapien. Ut felis diam,
+            malesuada vitae eros dictum, eleifend pretium risus. Praesent lobortis ante vitae rutrum tincidunt.
+            Quisque eu est molestie, feugiat sem aliquam, rutrum felis. Duis porta elit non fermentum pretium. Duis
+            porttitor risus eu varius vehicula.
+        </p>
+
+        <h2 class="mt-5 mb-2">This is another header</h2>
+        <p class="mt-3">
+            Sed efficitur eros vitae risus eleifend, non consequat velit porttitor. Sed feugiat tempus purus, at
+            lacinia nibh lobortis eget. Aenean quis mauris vel arcu faucibus scelerisque. Quisque quis condimentum sem.
+            Duis feugiat dolor in consequat mattis. Suspendisse ullamcorper ipsum vitae cursus ultrices. Aliquam mollis
+            mauris vitae neque varius, at elementum ex rutrum. Maecenas lacinia gravida sapien nec dictum. Quisque
+            elementum libero at felis auctor tincidunt. Ut ornare finibus justo eu eleifend. Curabitur et neque ut ex
+            consequat tincidunt. Duis id commodo urna. Nam tempus congue ante, at eleifend justo mattis vel. Praesent
+            id odio mi. Mauris commodo neque sed massa hendrerit, eget euismod sem vehicula. In quis vulputate odio.
+        </p>
     </div>
 @endsection
--- tailwind.js
+++ tailwind.js
@@ -195,7 +195,7 @@ module.exports = {
 
   fonts: {
     'sans': [
-      'Montserrat',
+      'Oswald',
       'system-ui',
       'BlinkMacSystemFont',
       '-apple-system',
@@ -210,7 +210,7 @@ module.exports = {
       'sans-serif',
     ],
     'serif': [
-      'Roboto Slab',
+      'EB Garamond',
       'Constantia',
       'Lucida Bright',
       'Lucidabright',
@@ -254,15 +254,15 @@ module.exports = {
   */
 
   textSizes: {
-    'xs': '.75rem',     // 12px
-    'sm': '.875rem',    // 14px
-    'base': '1rem',     // 16px
-    'lg': '1.125rem',   // 18px
-    'xl': '1.25rem',    // 20px
-    '2xl': '1.5rem',    // 24px
-    '3xl': '1.875rem',  // 30px
-    '4xl': '2.25rem',   // 36px
-    '5xl': '3rem',      // 48px
+    'xs': '.875rem',     // 14px
+    'sm': '1rem',        // 16px
+    'base': '1.125rem',  // 18px
+    'lg': '1.25rem',     // 20px
+    'xl': '1.5rem',      // 24px
+    '2xl': '1.875rem',   // 30px
+    '3xl': '2.25rem',    // 36px
+    '4xl': '3rem',       // 48px
+    '5xl': '3.5rem',     // 56px
   },
 
 
