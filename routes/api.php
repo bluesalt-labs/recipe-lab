da4060accbcb263940210d48b1d4b7be97352632
Unknown <luke@bluesaltlabs.com>
2019-03-31 14:15:18 -0700

    Adds to and reorganizes API routes.

--- routes/api.php
+++ routes/api.php
@@ -25,29 +25,62 @@ use Illuminate\Http\Request;
         // Cost Units
         Route::apiResource('cost-units', 'CostUnitController');
 
-        // Ingredients
-        Route::apiResource('ingredients', 'IngredientController');
+        // Measure Units
+        Route::apiResource('measure-units', 'MeasureUnitController');
 
-        // Ingredient Categories
-        Route::apiResource('ingredient-types', 'IngredientTypeController');
+        // Merchants
+        Route::apiResource('merchants', 'MerchantController');
+
+        // Merchant Products
+        Route::apiResource('merchant-products', 'MerchantProductController');
+        Route::apiResource('merchants/{merchant}/products', 'MerchantProductController');
 
         // Ingredient Categories
         Route::apiResource('ingredient-categories', 'IngredientCategoryController');
 
+        // Ingredient Types
+        Route::apiResource('ingredient-types', 'IngredientTypeController');
+        Route::apiResource('ingredient-categories/{category}/types', 'IngredientTypeController');
+
+        // Ingredients
+        Route::apiResource('ingredients', 'IngredientController');
+        Route::apiResource('ingredient-categories/{category}/ingredients', 'IngredientController');
+        Route::apiResource('ingredient-types/{type}/ingredients', 'IngredientController');
+        Route::apiResource('ingredient-categories/{category}/types/{type}/ingredients', 'IngredientController');
+
         // Ingredient Costs
         Route::apiResource('ingredient-costs', 'IngredientCostController');
+        Route::apiResource('ingredients/{ingredient}/costs', 'IngredientCostController');
+        Route::apiResource('ingredient-categories/{category}/ingredients/{ingredient}/costs', 'IngredientCostController');
+        Route::apiResource('ingredient-types/{type}/ingredients/{ingredient}/costs', 'IngredientCostController');
+        Route::apiResource('ingredient-categories/{category}/types/{type}/ingredients/{ingredient}/costs', 'IngredientCostController');
 
-        // Measure Units
-        Route::apiResource('measure-units', 'MeasureUnitController');
+        // Recipe Categories
+        Route::apiResource('recipe-categories', 'RecipeCategoryController');
+
+        // Recipe Types
+        Route::apiResource('recipe-types', 'RecipeTypeController');
+        Route::apiResource('recipe-categories/{category}/types', 'RecipeTypeController');
 
         // Recipes
         Route::apiResource('recipes', 'RecipeController');
-
-        // Recipe Steps
-        Route::apiResource('recipe-steps', 'RecipeStepController');
+        Route::apiResource('recipe-categories/{category}/recipes', 'RecipeController');
+        Route::apiResource('recipe-types/{type}/recipes', 'RecipeController');
+        Route::apiResource('recipe-categories/{category}/types/{type}/recipes', 'RecipeController');
 
         // Recipe Ingredients
-        Route::apiResource('recipe-ingredients', 'RecipeIngredientController');
+        //Route::apiResource('recipe-ingredients', 'RecipeIngredientController');
+        Route::apiResource('recipes/{recipe}/ingredients', 'RecipeIngredientController');
+        Route::apiResource('recipe-categories/{category}/recipes/{recipe}/ingredients', 'RecipeIngredientController');
+        Route::apiResource('recipe-types/{type}/recipes/{recipe}/ingredients', 'RecipeIngredientController');
+        Route::apiResource('recipe-categories/{category}/types/{type}/recipes/{recipe}/ingredients', 'RecipeIngredientController');
+
+        // Recipe Steps
+        //Route::apiResource('recipe-steps', 'RecipeStepController');
+        Route::apiResource('recipes/{recipe}/steps', 'RecipeStepController');
+        Route::apiResource('recipe-categories/{category}/recipes/{recipe}/steps', 'RecipeStepController');
+        Route::apiResource('recipe-types/{type}/recipes/{recipe}/ingredients', 'RecipeStepController');
+        Route::apiResource('recipe-categories/{category}/types/{type}/recipes/{recipe}/ingredients', 'RecipeStepController');
 
     });
 
